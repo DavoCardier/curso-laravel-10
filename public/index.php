d3afdcf8ff0b056cebe0d4a505dc30c7ef1661b2
David Cardier <yeiicardier@gmail.com>
2024-09-25 10:23:53 -0400

    Version 0.2

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+//var_dump($_SERVER['REQUEST_URI']); //o $_SERVER
+//die();
+
 define('LARAVEL_START', microtime(true));
 
 /*
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// get = Realiza una peticion de forma http seguido de la url 
+//
+
+Route::get('/home', function () {
+    return "Pagina de inicio";
 });
+
+Route::get('/notes/{id}', function($id) {
+    return 'Detalles de la note' .' '. $id;
+
+})->whereNumber('id'); // Con esta expresion lo que hacemos es engadenar la URL Con (whereNumber), Esto puede variar
+
+Route::get('/notes', function() {
+    return 'Listado de Notas';
+});
+
+Route::get('/notes/create', function () {
+    return 'Crear nueva nota';
+});
+
+Route::get('notas/{id}/editar', function ($id) {
+    return 'Editar nota: ' . $id;
+}); //Esta URL, permite Diferentes valores ya que no esta encadenada
+
+
